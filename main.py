eff261181d3985cf222d3d06e57860928a989f69
for-lessonn <holmonbek@gmail.com>
2024-09-18 09:32:05 +0500

    OOP

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Salom Kamron")
-print("Salom Abror")
+class Person:
+    def __init__(self, n, s, a):
+        self.name = n
+        self.surname = s
+        self.age = a
 
-print("Bugungi mavzu OOP")
+    def salom_ber(self, n):
+        print(f"Salom {n}")
+
+c1 = Person("Ibrohim", "Qo'ldoshev", 18)
+c2 = Person("Diyorbek", "Turdialiyev", 19)
+
+c1.salom_ber("Abror")
+c2.salom_ber("Shohruh")
