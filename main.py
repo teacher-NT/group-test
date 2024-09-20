47a43339b1b4e2deceb6cc82cf72bcbfefc65771
for-lessonn <holmonbek@gmail.com>
2024-09-20 09:47:17 +0500

    Abstraktsiya

--- main.py
+++ main.py
@@ -1,27 +1,84 @@
-class BankAccount:
-    def __init__(self, account_holder):
-        self.account_holder = account_holder  
-        self.__balance = 1000            
+from abc import ABC, abstractmethod
 
-    def get_balance(self):
-        return self.__balance
+class Player(ABC):
+    @abstractmethod
+    def run(self):
+        pass
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
-            print(f"{amount} miqdorda depozit kiritildi. Joriy balans: {self.__balance}")
-        else:
-            print("Depozit miqdori noto'g'ri!")
+    @abstractmethod
+    def turn(self):
+        pass
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
-            self.__balance -= amount
-            print(f"{amount} miqdorda pul yechildi. Joriy balans: {self.__balance}")
-        else:
-            print("Yechish uchun balans yetarli emas yoki miqdor noto'g'ri!")
+class BMW(Player):
+
+    def __init__(self, nom, rang, max_speed):
+        self.name = nom
+        self.color = rang
+        self.max_speed = max_speed
+
+    def run(self):
+        print("Running")
+
+    def turn(self):
+        print("Turn around")
+
+b = BMW("R8", "Qora", 260)
+
+print(b.max_speed)
 
 
-my_account = BankAccount("Elon Musk")
-print(my_account.get_balance()) 
-my_account.deposit(-200)  
-my_account.withdraw(5000)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class BankAccount:
+#     def __init__(self, account_holder):
+#         self.account_holder = account_holder  
+#         self.__balance = 1000            
+
+#     def get_balance(self):
+#         return self.__balance
+
+#     def deposit(self, amount):
+#         if amount > 0:
+#             self.__balance += amount
+#             print(f"{amount} miqdorda depozit kiritildi. Joriy balans: {self.__balance}")
+#         else:
+#             print("Depozit miqdori noto'g'ri!")
+
+#     def withdraw(self, amount):
+#         if 0 < amount <= self.__balance:
+#             self.__balance -= amount
+#             print(f"{amount} miqdorda pul yechildi. Joriy balans: {self.__balance}")
+#         else:
+#             print("Yechish uchun balans yetarli emas yoki miqdor noto'g'ri!")
+
+
+# my_account = BankAccount("Elon Musk")
+# print(my_account.get_balance()) 
+# my_account.deposit(-200)  
+# my_account.withdraw(5000)  
