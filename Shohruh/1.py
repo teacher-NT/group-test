008e66f4c24af80d384b355ea4f6a09221283047
Shohruh Abdullayev <abdullayevshohruh343@gmail.com>
2024-09-20 09:50:04 +0500

    bank getter va setter

--- Shohruh/1.py
+++ Shohruh/1.py
@@ -1,1 +1,13 @@
-print("hello")
+#print("hello")
+# import googletrans
+
+# translator = googletrans.Translator()
+
+# text = input("Enter text: ")
+
+# result = translator.translate(text, dest="en")
+
+# print(result.text)
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
