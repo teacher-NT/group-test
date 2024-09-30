1867adc49ca9a6b380b89d6bda7216ec7c6328eb
Shohruh Abdullayev <abdullayevshohruh343@gmail.com>
2024-09-30 09:22:50 +0500

    y

--- Shohruh/layoutlar.py
+++ Shohruh/layoutlar.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel,
     QPushButton, QLineEdit, QTextEdit,
-    QHBoxLayout, QVBoxLayout
+    QHBoxLayout, QVBoxLayout,
+    QMessageBox, QComboBox
 )
 from PyQt5.QtGui import QFont
 font = QFont("Arial", 14)
@@ -14,42 +15,23 @@ class Window(QWidget):
 
         layout = QVBoxLayout()
         self.setWindowTitle(title)
-        # self.setFixedSize(width, height)
+        self.setFixedSize(width, height)
         self.label = QLabel(self)
-        self.label.setText("Bu label")
+        self.label.setText("Menyu:")
         self.label.setFont(font)
 
         self.btn = QPushButton(self)
         self.btn.setText("Zoom")
-        # self.btn.move(50, 100)
         self.btn.clicked.connect(self.func)
-
-        self.btn1 = QPushButton(self)
-        self.btn1.setText("Zoom")
        
-
-        self.btn2 = QPushButton(self)
-        self.btn2.setText("Zoom")
-
-        self.btn3 = QPushButton(self)
-        self.btn3.setText("Zoom")
-       
+        self.menu = QComboBox(self)
+        self.menu.addItems(["Lavash", "HotDog", "Pizza", "Sho'rva", "Free", "Osh", "Shashlik"])
+        # self.menu.addItem("Lavash")
+        # self.menu.addItem("HotDog")
 
         layout.addWidget(self.label)
+        layout.addWidget(self.menu)
         layout.addWidget(self.btn)
-        layout.addWidget(self.btn1)
-        layout.addWidget(self.btn2)
-        layout.addWidget(self.btn3)
+        
         
         self.setLayout(layout)
-
-    def func(self):
-        self.label.setFixedSize(1200, 720)
-
-win = Window("OOP", 800, 600)
-win.show()
-
-
-
-
-app.exec_()
